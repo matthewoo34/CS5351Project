96a962f6676d6d8b304445d893ec056c1a271392
verybighub <verybighub@gmail.com>
2020-09-27 00:35:59 +0800

    Backlog #3 updateIssue method

--- src/main/java/ch/bsgroup/scrumit/controller/IssueController.java
+++ src/main/java/ch/bsgroup/scrumit/controller/IssueController.java
@@ -11,11 +11,14 @@ import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ch.bsgroup.scrumit.domain.Issue;
+import ch.bsgroup.scrumit.pojo.SerializableProject;
 import ch.bsgroup.scrumit.service.IIssueService;
 
 @Controller
@@ -33,7 +36,7 @@ public class IssueController {
 		this.issueService = issueService;
 	}
 	
-	@RequestMapping()
+	@RequestMapping(value="", method=RequestMethod.POST)
 	public @ResponseBody Map<String, ? extends Object> addIssue(@RequestBody Issue issue, HttpServletResponse response) {
 		Set<ConstraintViolation<Issue>> failures = validator.validate(issue);
 		if (!failures.isEmpty()) {
@@ -41,6 +44,22 @@ public class IssueController {
 			return validationMessages(failures);
 		} else {
 			return Collections.singletonMap("project", this.issueService.addIssue(issue));
+		}
+	}
+	
+	@RequestMapping(value="{issueId}", method=RequestMethod.PUT)
+	public @ResponseBody Map<String, ? extends Object> updateIssue(@RequestBody Issue i,HttpServletResponse response,@PathVariable int issueId) {
+		Set<ConstraintViolation<Issue>> failures = validator.validate(i);
+		Issue issue = this.issueService.findIssueById(issueId);
+		issue.setCategory(i.getCategory());
+		issue.setDescription(i.getDescription());
+		issue.setExtraDuration(i.getExtraDuration());
+		if (!failures.isEmpty()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return validationMessages(failures);
+		} else {
+			this.issueService.updateIssue(issue);
+			return Collections.singletonMap("issue", null);
 		}
 	}
 	
