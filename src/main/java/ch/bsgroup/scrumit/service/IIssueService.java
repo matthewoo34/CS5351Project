3774b17364ed6578b45f05b6dc0afa8774801e69
VictorYam31 <70901229+VictorYam31@users.noreply.github.com>
2020-09-26 23:53:04 +0800

    Add files via upload

---
+++ src/main/java/ch/bsgroup/scrumit/service/IIssueService.java
@@ -0,0 +1,17 @@
+package ch.bsgroup.scrumit.service;
+
+import ch.bsgroup.scrumit.domain.Issue;
+
+import java.util.Set;
+
+/**
+ * Person Service Interface
+ */
+public interface IIssueService {
+	public Issue addIssue(Issue issue);
+	public void updateIssue(Issue issue);
+	public void removeIssue(Issue issue);
+	public Set<Issue> getAllIssues();
+	public Issue findIssueById(int issueId);
+	public Set<Issue> getAllIssuesByProjectId(int projjectId);
+}
