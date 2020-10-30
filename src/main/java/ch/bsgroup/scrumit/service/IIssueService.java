2cf7bb174d6dbf5f9081e060ad03124dd6f2ddde
VictorYam31 <70901229+VictorYam31@users.noreply.github.com>
2020-10-30 22:49:14 +0800

    Update Issues and Big fix

---
+++ src/main/java/ch/bsgroup/scrumit/service/IIssueService.java
@@ -0,0 +1,18 @@
+package ch.bsgroup.scrumit.service;
+
+import ch.bsgroup.scrumit.domain.Issue;
+
+import java.util.Set;
+
+/**
+ * Person Service Interface
+ */
+public interface IIssueService {
+	public Issue addIssue(Issue issue);
+	public void updateIssue(Issue issue);
+	public void removeIssue(Issue issue);
+	public Set<Issue> getAllIssues();
+	public Issue findIssueById(int issueId);
+	public Set<Issue> getAllIssuesByProjectId(int projectId);
+	public Set<Issue> getAllIssuesByTaskId(int taskId);
+}
